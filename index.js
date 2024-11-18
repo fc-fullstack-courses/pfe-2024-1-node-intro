5c3545a9dd93a253cdf76a207f1ce2e1940adf63
Kyrylo <fresh.js.ftd.@gmail.com>
2024-11-18 20:29:14 +0200

    Add ESModules syntax example

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+// імпорт даних з інших модулей
+
+// 1. імпорт за замовчанням
+// import MyMath from './myMath.js';
+
+// при імпорті за замовчанням назва необов'язково має спвпадати
+// import TheBestMath from './myMath.js';
+
+// // 2. іменний імпорт
+// import { PIValue, testFunction as newTest,  } from './myMath.js';
+
+// // 3. Комбінований імпорт
+// import MyMath, { MyBoolAlgerbra, MyTrigonometryMath } from './myMath.js';
+
+import TheBestMath, { PIValue, testFunction as newTest, MyBoolAlgerbra, MyTrigonometryMath } from './myMath.js';
+
+import * as AllExportsFromMyMath from './myMath.js';
+
 console.log('Hello from Node');
 
-console.log(0.1 + 0.2);
+console.log(AllExportsFromMyMath);
 
-const sum = (...nums) => nums.reduce((acc, num) => acc + num);
+console.log(PIValue);
+
+newTest();
+
+function testFunction () {
+
+}
 
 const numbers = [5, 7, 8, 9, 10, -500];
 
-console.log(sum(...numbers));
+console.log(TheBestMath.sum(...numbers));
---
+++ myMath.js
@@ -0,0 +1,43 @@
+class MyMath {
+  static sum = (...nums) => nums.reduce((acc, num) => acc + num);
+
+  static subtract = (...nums) => nums.reduce((acc, num) => acc - num);
+
+  static multiply = (...nums) => nums.reduce((acc, num) => acc * num);
+
+  static divide = (...nums) => nums.reduce((acc, num) => acc / num);
+}
+
+class MyTrigonometryMath {
+  static func1 = () => {}
+}
+
+class MyBoolAlgerbra {
+  static func2 = () => {}
+}
+
+// експорт даних з модуля
+
+// 1. експорт за замовчанням - може використовуватися один раз за модуль
+export default MyMath;
+
+// export default 10;
+
+// const obj = {
+//   // MyTrigonometryMath: MyTrigonometryMath,
+//   MyTrigonometryMath,
+// }
+
+// obj.MyTrigonometryMath
+
+// 2. іменний експорт
+export {
+  MyTrigonometryMath,
+  MyBoolAlgerbra
+};
+
+export const PIValue = 3.14;
+
+export function testFunction () {
+  console.log('testFunction is executed');
+}
